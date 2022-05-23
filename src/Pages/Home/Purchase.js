8ec944080ad9f5f662c76c1e4d5fee152d17dfc6
akram hoque <oriniusakram511@gmail.com>
2022-05-24 01:05:40 +0600

    addedd env local

--- src/Pages/Home/Purchase.js
+++ src/Pages/Home/Purchase.js
@@ -1,16 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const Purchase = () => {
   
   const { itemId } = useParams();
   const [item, setItem] = useState({});
+ 
   useEffect(() => {
     const url = `http://localhost:5000/purchase/${itemId}`;
     fetch(url)
         .then(res => res.json())
         .then(data => setItem(data));
 }, [item])
+
+const [user, loading, error] = useAuthState(auth);
+
+const handlePlaceOrder = event =>{
+  event.preventDefault();
+  console.log('submit')
+  const order = {
+    productName: event.target.product.value,
+    order: event.target.order.value,
+    email: user.email,
+    customerName: user.displayName,
+    address: event.target.address.value,
+    phoneNumber: event.target.phone.value
+
+  }
+  console.log(order);
+}
   return (
     <div>
       <h2 className='text-3xl font-bold text-violet-500  my-5 '>Your Selected Items</h2>
@@ -23,6 +43,17 @@ const Purchase = () => {
        <p className="font-bold">Price: $ {item?.price}</p>
        <p className="font-bold">Available: {item?.availableQuantity}</p>
        <p className="font-bold">Min Order: {item?.minOrderQuantity}</p>
+
+       <form onSubmit={handlePlaceOrder} className='grid grid-cols-1 gap-3 justify-items-center mt-1'>
+       <input type="text" name='product' placeholder='Product Name'  class="input input-bordered input-red-500 w-full max-w-xs" />
+       <input type="text" name='order' placeholder='Order' class="input input-bordered input-red-500 w-full max-w-xs" />
+       <input type="text" name='name' value={user?.displayName || ''} disabled class="input input-bordered input-red-500 w-full max-w-xs" />
+       <input type="email" name='email' value={user?.email || ''}  disabled class="input input-bordered input-red-500 w-full max-w-xs" />
+       <input type="text" name='address' placeholder="Your Address" class="input input-bordered input-red-500 w-full max-w-xs" />
+       <input type="text" name='phone' placeholder="Phone Number" class="input input-bordered input-red-500 w-full max-w-xs" />
+       <input type="submit" value='Submit' class=" btn btn-success w-full max-w-xs" />
+       </form>
+       
      </div>
    </div>
     </div>
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,15 +6,15 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCSFFV5WMoTx9hmXij5gmxz-btQDsIIkqw",
-  authDomain: "assignment-12-53a87.firebaseapp.com",
-  projectId: "assignment-12-53a87",
-  storageBucket: "assignment-12-53a87.appspot.com",
-  messagingSenderId: "290752209911",
-  appId: "1:290752209911:web:b10576e0767daae948c4f2"
+  apiKey:process.env.REACT_APP_API_KEY,
+  authDomain:process.env.REACT_APP_AUTH_DOMAIN,
+  projectId:process.env.REACT_APP_PROJECT_ID,
+  storageBucket:process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId:process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId:process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+const auth = getAuth(app)
 export default auth ;
