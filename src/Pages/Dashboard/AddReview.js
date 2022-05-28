7f22fcdfb369643c7248565059b44ff944025c30
akram hoque <oriniusakram511@gmail.com>
2022-05-28 18:25:07 +0600

    added final file

--- src/Pages/Dashboard/AddReview.js
+++ src/Pages/Dashboard/AddReview.js
@@ -1,13 +1,23 @@
 import React, { useState } from 'react';
 
 const AddReview = () => {
- 
+  const addReview = event => {
+    event.preventDefault();
+
+    const rating = event.target.rating.value;
+    const comment = event.target.comment.value;
+    console.log(rating, comment);
+  }
   return (
     <div>
-      <h2>This is Review</h2>
-     
+      <h2 className='text-center text-xl text-slate-500'>Add Review </h2>
+      <form onSubmit={addReview} className='grid grid-cols-1 gap-3 justify-items-center mt-1'>
+            <input type="text" name='rating' placeholder='Your ratings' class="input input-bordered input-red-500 w-full max-w-xs" />
+            <textarea name="comment" id="" cols="30" rows="10" placeholder='write here...'></textarea>
+            <input  type="submit" value='Add Review' class=" btn btn-success w-full max-w-xs" />
+          </form>
     </div>
   );
 };
 
-export default AddReview;
+export default AddReview ;
